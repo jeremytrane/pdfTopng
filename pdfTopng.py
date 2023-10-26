a10f8bc7a2308c7ed3666658ac261ab8284b9fe9
jeremytrane <jeremy.trane@gmail.com>
2023-10-26 14:47:13 +0100

    Added threading

--- pdfTopng.py
+++ pdfTopng.py
@@ -1,9 +1,10 @@
 import sys
 import tkinter as tk
-from tkinter import messagebox, filedialog
+from tkinter import ttk, messagebox, filedialog
 from pdf2image import convert_from_path
 import PyPDF2
 import os
+import threading
 
 def get_pdf_page_count(pdf_path):
     """Return the number of pages in the given PDF."""
@@ -12,37 +13,21 @@ def get_pdf_page_count(pdf_path):
         print(f"Number of pages in the pdf: {len(pdf.pages)}")
         return len(pdf.pages)
 
-def convert_pdf(output_dir):
-    if not pdf_file_path.get():
-        messagebox.showerror("Error", "No PDF file provided!")
-        return
-
-    # Fetch the user input page number
+def convert_pdf(output_dir, page_num, pdf_path, progress_var):
     try:
-        page_num = int(page_number_entry.get()) - 1
-        print(f"Page to convert: {page_num + 1}")
-    except ValueError:
-        messagebox.showerror("Error", "Invalid page number!")
-        return
+        images = convert_from_path(pdf_path, first_page=page_num + 1, last_page=page_num + 1)
 
-    path = pdf_file_path.get()
-    print(f"Path to convert: {path}")
-    
-    # Check if the provided page number is within the range
-    total_pages = get_pdf_page_count(path)
-    if page_num < 0 or page_num >= total_pages:
-        messagebox.showerror("Error", f"Page number out of range! PDF has {total_pages} pages.")
-        return
+        # Construct the output file path with the 'Downloads' folder
+        output_file_path = os.path.join(output_dir, f'output_page_{page_num + 1}.png')
 
-    images = convert_from_path(path)
+        # Save the image to the specified path
+        images[0].save(output_file_path, 'PNG')
 
-    # Construct the output file path with the 'Downloads' folder
-    output_file_path = os.path.join(output_dir, f'output_page_{page_num + 1}.png')
-
-    # Save the image to the specified path
-    images[page_num].save(output_file_path, 'PNG')
-    
-    messagebox.showinfo("Info", "Conversion Complete!")
+        messagebox.showinfo("Info", f"Conversion of page {page_num + 1} complete!")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {str(e)}")
+    finally:
+        progress_var.set(100)  # Update the progress bar to completion
 
 def select_pdf():
     file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
@@ -57,11 +42,38 @@ def select_output_directory():
     if output_dir:
         output_dir_label.config(text=output_dir)
 
+def start_conversion():
+    if not pdf_file_path.get():
+        messagebox.showerror("Error", "No PDF file provided!")
+        return
+
+    try:
+        page_num = int(page_number_entry.get()) - 1
+        print(f"Page to convert: {page_num + 1}")
+    except ValueError:
+        messagebox.showerror("Error", "Invalid page number!")
+        return
+
+    path = pdf_file_path.get()
+    print(f"Path to convert: {path}")
+
+    # Check if the provided page number is within the range
+    total_pages = get_pdf_page_count(path)
+    if page_num < 0 or page_num >= total_pages:
+        messagebox.showerror("Error", f"Page number out of range! PDF has {total_pages} pages.")
+        return
+
+    # Create and start a new thread for the conversion
+    progress_var.set(0)  # Reset progress bar
+    conversion_thread = threading.Thread(target=convert_pdf, args=(output_dir, page_num, path, progress_var))
+    conversion_thread.start()
+
 root = tk.Tk()
 root.title("PDF to PNG Converter")
 
 pdf_file_path = tk.StringVar()
 output_dir = ""
+progress_var = tk.IntVar()
 
 btn_select = tk.Button(root, text="Select PDF", command=select_pdf)
 btn_select.pack(pady=10)
@@ -83,9 +95,13 @@ btn_select_output_dir.pack(pady=10)
 output_dir_label = tk.Label(root, text="", font=("Arial", 10))
 output_dir_label.pack(pady=5)
 
-btn_convert = tk.Button(root, text="Convert", command=lambda: convert_pdf(output_dir))
+btn_convert = tk.Button(root, text="Convert", command=start_conversion)
 btn_convert.pack(pady=20)
 
-root.geometry("300x300")
+# Add a progress bar to show conversion progress
+progress_bar = ttk.Progressbar(root, variable=progress_var, maximum=100)
+progress_bar.pack(fill=tk.X, padx=10, pady=10)
+
+root.geometry("600x400")  # Set the window width and height
 
 root.mainloop()
